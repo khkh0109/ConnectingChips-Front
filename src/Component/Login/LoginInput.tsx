9fbb1ab7c7b26451546d448487de36f348d9d935
khkh0109 <ist200120@gmail.com>
2024-02-20 21:53:45 +0900

    🔨 Refactor: LoginInput 컴포넌트 분리
    해당 컴포넌트를 바로 찾기 힘들어서 한 파일 안에 따로 정의되어 있는 LoginInput 컴포넌트를 분리함

---
+++ src/Component/Login/LoginInput.tsx
@@ -0,0 +1,48 @@
+import { LoginInputS } from '../../StyleComp/LoginInputS';
+
+// TODO: 중복된 타입임
+type bindValue = {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
+interface LoginInputProps {
+  sort: 'ID' | 'PW';
+  isdefault: boolean;
+  inputbind: bindValue;
+  setInputState: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const LoginInput = ({
+  sort,
+  isdefault,
+  inputbind,
+  setInputState,
+}: LoginInputProps): JSX.Element => {
+  const { value, setValue } = inputbind;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    setInputState('default');
+  };
+
+  if (sort === 'ID')
+    return (
+      <LoginInputS
+        placeholder='아이디를 입력해 주세요'
+        className={isdefault ? '' : 'failed'}
+        value={value}
+        onChange={handleInputChange}
+      />
+    );
+
+  return (
+    <LoginInputS
+      placeholder='비밀번호를 입력해 주세요'
+      className={isdefault ? '' : 'failed'}
+      type={true ? 'password' : 'text'}
+      value={value}
+      onChange={handleInputChange}
+    />
+  );
+};
