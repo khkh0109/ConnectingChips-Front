7cb22822a5cc2a68ad39a70f9e8b50bde0b58fe7
khkh0109 <ist200120@gmail.com>
2024-02-26 14:40:30 +0900

    🔨 Refactor: SignUpLinkS 내부 구조 변경 및 관련 css 수정
     1. 오른쪽 화살표 이미지를 svg로 불러옴(ArrowRight 컴포넌트)
    이에 따라 img 태그와 img 태그를 감싸는 div 태그 삭제, 사용하지 않는 이미지 import (Arrow_Right)도 삭제함
    
     2. ArrowRight 컴포넌트를 Link 내부로 이동
    오른쪽 화살표 눌렀을때는 링크가 작동하지 않아서 이동시킴
    
    3. 필요없는 css 속성 삭제
    SignUpLinkS 구조 변경으로 인해 필요없어진 css 코드 삭제함

--- src/Page/LogIn/LoginBarrel.ts
+++ src/Page/LogIn/LoginBarrel.ts
@@ -1,9 +1,9 @@
 import Banner from '../../Component/SignUp/Banner';
 import { GroupHeader } from '../../Component/Mission/GroupHeader';
-import { Arrow_Right } from '../../Component/ArrowBarrel';
+import { Arrow_Right as ArrowRight } from '../../Component/ArrowBarrel';
 
 import { SignClearBtnS } from '../../StyleComp/SignBtnS';
 
 export * from '../../StyleComp/LoginInputS';
 export * from '../../API/login';
-export { GroupHeader, Banner, SignClearBtnS, Arrow_Right };
+export { GroupHeader, Banner, SignClearBtnS, ArrowRight };
