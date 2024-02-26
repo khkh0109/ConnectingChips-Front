3713ae6c04a20b7547f7e1ebec93a7ad761cf8e4
khkh0109 <ist200120@gmail.com>
2024-02-26 15:25:49 +0900

    🔨 Refactor: Login 컴포넌트에서 입력과 전송 기능을 하는 form 부분 분리
    1. Login 컴포넌트에서 LoginForm 분리
     Login 컴포넌트 내부가 복잡해서 페이지 컴포넌트 구조를 바로 파악하기 힘들었음. 그래서 입력과 전송 기능인 form 부분을 LoginForm 컴포넌트로 분리. 또한 이렇게 분리하면 form 부분만 신경쓸 수 있기 때문에 유지보수하기 편할것이라 판단함
    
    2. 컴포넌트 분리에 따른 LoginBarrel.ts 파일 정리
     LoginForm.tsx에서 import 해야 하는 부분 삭제함

--- src/Page/LogIn/LoginBarrel.ts
+++ src/Page/LogIn/LoginBarrel.ts
@@ -1,11 +1,13 @@
 import Banner from '../../Component/SignUp/Banner';
 import { GroupHeader } from '../../Component/Mission/GroupHeader';
-import { ReactComponent as ArrowRight } from '../../image/Icon/Arrow/Arrow_icon_Right.svg';
 import { LoginInput } from '../../Component/Login/LoginInput';
+import LoginForm from '../../Component/Login/LoginForm';
 
 import { SignClearBtnS } from '../../StyleComp/SignBtnS';
+
+import { ReactComponent as ArrowRight } from '../../image/Icon/Arrow/Arrow_icon_Right.svg';
 
 export * from '../../StyleComp/LoginInputS';
 export * from '../../API/login';
 
-export { Banner, GroupHeader, SignClearBtnS, ArrowRight, LoginInput };
+export { Banner, GroupHeader, SignClearBtnS, ArrowRight, LoginInput, LoginForm };
