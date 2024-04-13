069414a3d55740180e74588276362272d133a13f
khkh0109 <ist200120@gmail.com>
2024-04-13 15:13:34 +0900

    💬 Comment: 사용하지 않는 주석 삭제

--- src/Page/LogIn/LogIn.tsx
+++ src/Page/LogIn/LogIn.tsx
@@ -1,7 +1,6 @@
 import { useState, Link, styled, useNavigate } from './LoginBarrel';
 import { LogInS, LoginInputS, SignClearBtnS, Arrow_Right } from './LoginBarrel';
 import Banner from '../../Component/SignUp/Banner';
-// import useLoginCheck from '../../Hooks/useLoginCheck';
 import { postLogin } from '../../API/login';
 import { GroupHeader } from '../../Component/Mission/GroupHeader';
 
@@ -10,7 +9,6 @@ type bindValue = {
   setValue: React.Dispatch<React.SetStateAction<string>>;
 };
 
-/** 2023-08-24 LogIn.tsx - 로그인 페이지 */
 const LogIn = (): JSX.Element => {
   const [inputState, setInputState] = useState('default');
   const [nickname, setNickname] = useState('');
@@ -18,9 +16,6 @@ const LogIn = (): JSX.Element => {
   const idBind: bindValue = { value: nickname, setValue: setNickname };
   const pwBind: bindValue = { value: password, setValue: setPassword };
   const navigate = useNavigate();
-
-  // TODO: 로그인 여부는 라우터에서 처리중(사용하지 않으면 삭제하기)
-  // useLoginCheck(navigate, 'Done');
 
   const isDefault = inputState === 'default';
 
@@ -117,7 +112,6 @@ const LoginInput = ({
   );
 };
 
-/** 2023-08-24 LogIn.tsx - 로그인 / 회원가입 배너 */
 const LoginOuterContainerS = styled.div`
   display: flex;
   flex-direction: column;
@@ -125,7 +119,6 @@ const LoginOuterContainerS = styled.div`
   padding: 0 1rem;
 `;
 
-/** 2023-08-24 LogIn.tsx - 로그인 입력폼 */
 const LoginFormS = styled.form`
   display: flex;
   flex-direction: column;
@@ -142,7 +135,6 @@ const LoginContainerS = styled.div`
   margin-bottom: 0.97rem;
 `;
 
-/** 2023-08-24 LogIn.tsx - 로그인 입력 컨테이너 */
 const LoginInnerContainerS = styled.div`
   display: flex;
   flex-direction: column;
@@ -150,7 +142,6 @@ const LoginInnerContainerS = styled.div`
   margin-bottom: 0.47rem;
 `;
 
-/** 2023-08-24 LogIn.tsx - 회원가입 유도 */
 const NudgeSignS = styled.div`
   margin-top: 4.25rem;
   display: flex;
