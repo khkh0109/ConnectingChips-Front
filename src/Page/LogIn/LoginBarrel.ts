398df0d7595d10c2cab112b1c9c6a6960e4b98f1
khkh0109 <ist200120@gmail.com>
2024-04-13 17:04:05 +0900

    🔨 Refactor: LoginBarrel에서 사용하지 않는 모듈 삭제

--- src/Page/LogIn/LoginBarrel.ts
+++ src/Page/LogIn/LoginBarrel.ts
@@ -1,14 +1,9 @@
 import Banner from '../../Component/Banner';
 import { GroupHeader } from '../../Component/Mission/GroupHeader';
-import { LoginInput } from '../../Component/Login/LoginInput';
 import LoginForm from '../../Component/Login/LoginForm';
 import SignUpLink from '../../Component/Login/SignUpLink';
-
-import { SignClearBtnS } from '../../StyleComp/SignBtnS';
-
-import { ReactComponent as ArrowRight } from '../../image/Icon/Arrow/Arrow_icon_Right.svg';
 
 export * from '../../StyleComp/LoginInputS';
 export * from '../../API/login';
 
-export { Banner, GroupHeader, SignClearBtnS, ArrowRight, LoginInput, LoginForm, SignUpLink };
+export { GroupHeader, Banner, LoginForm, SignUpLink };
