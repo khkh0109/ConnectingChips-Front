d255c9481c0558718b60387d76df70a9f7d0ee3f
khkh0109 <ist200120@gmail.com>
2024-04-13 17:07:06 +0900

    🔨 Refactor: LoginBarrel 파일에서 사용하지 않는 모듈 삭제

--- src/Page/LogIn/LoginBarrel.ts
+++ src/Page/LogIn/LoginBarrel.ts
@@ -3,7 +3,4 @@ import { GroupHeader } from '../../Component/Mission/GroupHeader';
 import LoginForm from '../../Component/Login/LoginForm';
 import SignUpLink from '../../Component/Login/SignUpLink';
 
-export * from '../../StyleComp/LoginInputS';
-export * from '../../API/login';
-
 export { GroupHeader, Banner, LoginForm, SignUpLink };
