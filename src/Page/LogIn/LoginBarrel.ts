c62b53fbb68bfc3c73abb5af9c8b0f8d025513a2
khkh <77181642+khkh0109@users.noreply.github.com>
2024-04-21 02:03:30 +0900

    🔨 Refactor: Login 중복 파일 삭제

--- src/Page/LogIn/LoginBarrel.ts
+++
@@ -1,6 +0,0 @@
-import { GroupHeader } from '../../Component/Mission/GroupHeader';
-import Logo from '../../Component/Logo';
-import LoginForm from '../../Component/Login/LoginForm';
-import SignUpLink from '../../Component/Login/SignUpLink';
-
-export { GroupHeader, Logo, LoginForm, SignUpLink };
