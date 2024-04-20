a0cadc9f71d540fbae2044dea293caac754aade0
khkh0109 <ist200120@gmail.com>
2024-04-20 20:24:16 +0900

    🔨 Refactor: Logo 컴포넌트 padding, margin props 제거
    Logo 컴포넌트에는 로고와 관련된 스타일만 있는게 좋을 것 같아서 제거함

--- src/Component/Logo.tsx
+++ src/Component/Logo.tsx
@@ -6,18 +6,14 @@ import { ReactComponent as LogoDark } from '../image/logo/logo-dark.svg';
 interface LogoProps {
   mode: 'light' | 'dark';
   width: string;
-  padding?: string;
-  margin?: string;
 }
 
-export default function Logo({ mode, width, padding = '', margin = '' }: LogoProps) {
+export default function Logo({ mode, width }: LogoProps) {
   const LogoComponent = mode === 'light' ? LogoLight : LogoDark;
 
-  return <LogoS as={LogoComponent} width={width} height='100%' padding={padding} margin={margin} />;
+  return <LogoS as={LogoComponent} width={width} height='100%' />;
 }
 
-const LogoS = styled.svg<{ margin: string; padding: string }>`
+const LogoS = styled.svg`
   display: block;
-  padding: ${({ padding }) => padding};
-  margin: ${({ margin }) => margin};
 `;
