115088012d2b7f6e2a9650d05356f05bc0c7d91a
khkh0109 <ist200120@gmail.com>
2024-11-28 12:08:25 +0900

    chore: msw 설정 및 샘플 코드 추가

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,11 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  // Sample Code...
+  http.get('https://api.example.com/user', () => {
+    return HttpResponse.json({
+      firstName: 'John',
+      lastName: 'Maverick',
+    });
+  }),
+];
