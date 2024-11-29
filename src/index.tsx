52f79ff9d6aed9a7aeb1978745cfa4b3d4926ee6
khkh0109 <ist200120@gmail.com>
2024-11-29 18:37:17 +0900

    chore: msw 서비스 워커 설정

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,31 @@ import { StyledToastContainer } from './Component/Toast/StyledToastContainer';
 if (process.env.REACT_APP_GOOGLE_ANALYTICS)
   ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-      <StyledToastContainer />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+
+enableMocking().then(() => {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+        <StyledToastContainer />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
